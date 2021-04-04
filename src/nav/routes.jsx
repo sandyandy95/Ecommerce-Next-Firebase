4e4fc06dfb237a2bd9695dffa78bdf62080b2483
CrisRonda <cristian.j005@gmail.com>
2021-04-03 22:42:56 -0500

    feat: change routes

--- src/nav/routes.jsx
+++ src/nav/routes.jsx
@@ -1,27 +1,25 @@
-import {
-  AvTimer,
-  LocalMall,
-  Person,
-  ShoppingCart,
-  SupervisorAccount,
-} from '@material-ui/icons';
+import { AvTimer, LocalMall, Person, ShoppingCart, SupervisorAccount } from '@material-ui/icons';
 
-const routes = ({ uid }) => [
-  { href: '/carrito', label: 'carrito', icon: <ShoppingCart /> },
-  { href: '/admin/usuarios', label: 'admin', icon: <SupervisorAccount /> },
-  { href: '/pedidos', label: 'pedidos', icon: <LocalMall /> },
-  {
-    href: '/pedidos/anteriores',
-    label: 'pedidos anteriores',
-    icon: <AvTimer />,
-  },
-  {
-    href: '/usuario/[uid]/productos',
-    // href: `/usuario/${user.uid}/productos`,
-    as: `/usuario/${uid}/productos`,
-    label: 'Perfil',
-    icon: <Person />,
-  },
-];
+const routes = ({ uid }) => ({
+  admin: [{ href: '/admin/usuarios', label: 'admin', icon: <SupervisorAccount /> }],
+  user: [
+    { href: '/carrito', label: 'carrito', icon: <ShoppingCart /> },
+    {
+      href: '/compras',
+      label: 'Compras',
+      icon: <AvTimer />,
+    },
+  ],
+  seller: [
+    { href: '/pedidos', label: 'pedidos', icon: <LocalMall /> },
+    {
+      href: '/usuario/[uid]/productos',
+      // href: `/usuario/${user.uid}/productos`,
+      as: `/usuario/${uid}/productos`,
+      label: 'Perfil',
+      icon: <Person />,
+    },
+  ],
+});
 
 export default routes;
