d37cc077d8ecfa10f2b1d71e1ea7414b0c700d6f
CrisRonda <cristian.j005@gmail.com>
2021-04-27 20:20:42 -0500

    test

--- pages/usuario/[uid]/productos.jsx
+++ pages/usuario/[uid]/productos.jsx
@@ -1,4 +1,25 @@
+import { getProductsById } from '../../../services/products/db';
 import Products from '../../../src/screens/Products';
 
-const Page = () => <Products />;
+const Page = (props) => <Products {...props} />;
+
+export const getServerSideProps = async (ctx) => {
+  const uid = '44717de7-1053-46b2-bcfe-b6315b615cba';
+  try {
+    const products = await getProductsById(uid);
+    if (products.length === 0) {
+      throw new Error('Usuario no existe');
+    }
+    return {
+      props: {
+        products: JSON.parse(JSON.stringify(products)),
+      },
+    };
+  } catch (error) {
+    const { res } = ctx;
+    res.statusCode = 302;
+    return res.setHeader('Location', '/404');
+  }
+};
+
 export default Page;
